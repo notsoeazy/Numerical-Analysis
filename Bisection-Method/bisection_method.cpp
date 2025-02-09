801cc026a9fcc710dd9d6cee54a1ecd48369c34f
notsoeazy <gonzalesjohnezekiel35@gmail.com>
2025-02-09 13:02:12 +0800

    moved method definition of class

--- Bisection-Method/bisection_method.cpp
+++ Bisection-Method/bisection_method.cpp
@@ -5,69 +5,9 @@
 class BisectionMethod
 {
 public:
-    BisectionMethod(){
-        numOfItr = 0;
-        accuracy = 0.0000000001;
-    };
-    void SetInterval(double (*f)(double)){
-        while(true){
-            std::cout << "Enter guess interval:\n";
-            std::cout << "Lower interval: ";
-            std::cin >> Xa;
-            std::cout << "Upper interval: ";
-            std::cin >> Xb;
-            if ((f(Xa) * f(Xb)) <= 0){
-                origLowInterval = Xa;
-                origUpInterval = Xb;
-                break;
-            }
-            else{
-                std::cout << "No roots in the interval, enter another interval...\n";
-            }
-        }
-    };
-    void SolveRoot(double (*f)(double)){
-        SetInterval(f);
-
-        std::cout << "#itr" << "\t" << "Xa" << "\t\t\t" << "f(Xa)" << "\t\t\t" << "Xb" << "\t\t\t" << "f(Xb)" << "\t\t\t" << "Xc" << "\t\t\t" << "f(Xc)" << std::endl;
-
-        while(fabs(Xa - Xb) > accuracy){
-            numOfItr++;
-
-            Xc = (Xa + Xb)/2;
-            fXa = f(Xa);
-            fXb = f(Xb);
-            fXc = f(Xc);
-
-            std::cout << numOfItr << "\t" << Xa << "\t\t" << fXa << "\t\t" << Xb << "\t\t" << fXb << "\t\t" << Xc << "\t\t" << fXc << std::endl;
-
-            //check if mayroon zero, matic root siya
-            if(fXa == 0){
-                Xc = Xa;
-                break;
-            }
-            if(fXb == 0){
-                Xc = Xb;
-                break;
-            }
-            if(fXc == 0){
-                break;
-            }
-
-            //swap intervals
-            if(fXa * fXc < 0){
-                Xb = Xc;
-            }
-            else{
-                Xa = Xc;
-            }
-        }
-        std::cout << "---------------------------------------------------------------------------\n";
-        std::cout << "Starting Interval: [" << origLowInterval << ", " << origUpInterval << "]" << std::endl;
-        std::cout << "Ending Interval: [" << Xa << ", " << Xb << "]" << std::endl;
-        std::cout << "Number of Iterations: " << numOfItr << std::endl;
-        std::cout << "Root is " << Xc << std::endl;
-    }
+    BisectionMethod();
+    void SetInterval(double (*f)(double));
+    void SolveRoot(double (*f)(double));
 private:
     int numOfItr;
     double Xa, Xb, Xc, fXa, fXb, fXc;
@@ -117,4 +57,70 @@ int main(){
     std::cout << "\n---------------------------------------------------------------------------\n";
 
     return 0;
+}
+
+BisectionMethod::BisectionMethod(){
+    numOfItr = 0;
+    accuracy = 0.0000000001;
+};
+
+void BisectionMethod::SetInterval(double (*f)(double)){
+    while(true){
+        std::cout << "Enter guess interval:\n";
+        std::cout << "Lower interval: ";
+        std::cin >> Xa;
+        std::cout << "Upper interval: ";
+        std::cin >> Xb;
+        if ((f(Xa) * f(Xb)) <= 0){
+            origLowInterval = Xa;
+            origUpInterval = Xb;
+            break;
+        }
+        else{
+            std::cout << "No roots in the interval, enter another interval...\n";
+        }
+    }
+};
+
+void BisectionMethod::SolveRoot(double (*f)(double)){
+    SetInterval(f);
+
+    std::cout << "#itr" << "\t" << "Xa" << "\t\t\t" << "f(Xa)" << "\t\t\t" << "Xb" << "\t\t\t" << "f(Xb)" << "\t\t\t" << "Xc" << "\t\t\t" << "f(Xc)" << std::endl;
+
+    while(fabs(Xa - Xb) > accuracy){
+        numOfItr++;
+
+        Xc = (Xa + Xb)/2;
+        fXa = f(Xa);
+        fXb = f(Xb);
+        fXc = f(Xc);
+
+        std::cout << numOfItr << "\t" << Xa << "\t\t" << fXa << "\t\t" << Xb << "\t\t" << fXb << "\t\t" << Xc << "\t\t" << fXc << std::endl;
+
+        //check if mayroon zero, matic root siya
+        if(fXa == 0){
+            Xc = Xa;
+            break;
+        }
+        if(fXb == 0){
+            Xc = Xb;
+            break;
+        }
+        if(fXc == 0){
+            break;
+        }
+
+        //swap intervals
+        if(fXa * fXc < 0){
+            Xb = Xc;
+        }
+        else{
+            Xa = Xc;
+        }
+    }
+    std::cout << "---------------------------------------------------------------------------\n";
+    std::cout << "Starting Interval: [" << origLowInterval << ", " << origUpInterval << "]" << std::endl;
+    std::cout << "Ending Interval: [" << Xa << ", " << Xb << "]" << std::endl;
+    std::cout << "Number of Iterations: " << numOfItr << std::endl;
+    std::cout << "Root is " << Xc << std::endl;
 }
