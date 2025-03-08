107bf27bc9ac5ba52a74edcac219ad4c5e91c3c6
notsoeazy <gonzalesjohnezekiel35@gmail.com>
2025-03-08 18:03:16 +0800

    fixes secant method

--- Secant-Method/secant-method.cpp
+++ Secant-Method/secant-method.cpp
@@ -1,97 +1,87 @@
 #include<iostream>
-#include<iomanip>
 #include<cmath>
+#include<iomanip>
 
-void SecantMethod(double (*ptr)(const double&)){
+
+void SecantMethod(double (*f)(const double& )){
+    const double accuracy = 1e-11;
     int itr = 0;
-    double Xi = NAN;
-    double XiPrev = NAN;
-    double fXi = NAN;
-    double fXiPrev = NAN;
-    double XiNew = NAN;
+    double xi, xiPrev, fxi, fxiPrev, xiNew;
     
-    double accuracy = 1e-10;
     std::cout.precision(10);
     std::cout.setf(std::ios::fixed);
+    std::cout.setf(std::ios::right);
 
-    do
-    {
+    do{
         std::cout << "Enter Xi-1: ";
-        std::cin >> XiPrev;
+        std::cin >> xiPrev;
         std::cout << "Enter Xi: ";
-        std::cin >> Xi;
+        std::cin >> xi;
+    } while(
+        [](const double& num1, const double& num2) -> bool{
+            if(num1 == num2){
+                std::cout << "Enter again...\n";
+                return true;
+            } else {return false;}
+        }(xiPrev, xi)
+    );
 
-    } while ([](double Xi, double XiPrev) -> bool{
-        if(Xi == XiPrev){
-            std::cout << Xi << XiPrev;
-            std::cout << "Enter another Interval...\n";
-            return true;
-        }
-        else 
-            return false;
-        }(Xi, XiPrev));
-    
-    std::cout << "#itr" << std::setw(10)
-              << "Xi-1" << std::setw(15)
-              << "Xi" << std::setw(15)
-              << "f(Xi-1)" << std::setw(15)
-              << "f(Xi)" << std::setw(15)
-              << "Xi+1\n";
+    std::cout << std::setw(5) << "itr"
+              << std::setw(20) << "Xi-1"
+              << std::setw(20) << "Xi"
+              << std::setw(20) << "f(Xi)"
+              << std::setw(20) << "f(Xi-1)"
+              << std::setw(20) << "Xi+1"
+              << std::endl;
 
-    do
-    {
-        fXiPrev = ptr(XiPrev);
-        fXi = ptr(Xi);
-        
-        XiNew = Xi - fXi * ((Xi - XiPrev)/(fXi - fXiPrev));
-
-        std::cout << ++itr << "\t" << XiPrev << "\t" << Xi << "\t" <<  fXiPrev << "\t" <<  fXi << "\t" <<  XiNew << "\n";
-
-        XiPrev = Xi;
-        Xi = XiNew;
-
-    } while (fabs(fXi) > accuracy);
-
-    std::cout << "\nIrations: " << itr << std::endl;
-    std::cout << "Root: " << XiNew << std::endl;
-};
-
+    do{
+        fxi = f(xi);
+        fxiPrev = f(xiPrev);
+        xiNew = xi - (fxi * (xi - xiPrev)/(fxi - fxiPrev));
+        std::cout << std::setw(5) << ++itr
+                  << std::setw(20) << xiPrev
+                  << std::setw(20) << xi
+                  << std::setw(20) << fxi
+                  << std::setw(20) << fxiPrev
+                  << std::setw(20) << xiNew
+                  << std::endl;
+        xiPrev = xi;
+        xi = xiNew;
+    } while(fabs(f(xiNew)) > accuracy);
+    std::cout << "\nThe root is " << xiNew 
+              << "\nFounf in " << itr << " iterations.\n";
+}
 
 // f(x) = x^5 -21x^2 -8x^3 -4x^4 -28x + 60
 double f1(const double& x){
     return pow(x, 5) - (21 * pow(x, 2)) - (8 * pow(x, 3)) - (4 * pow(x, 4)) - (28 * x) + 60;
 }
-
 // f(x) = x^3 - x^2 + e^x
 double f2(const double& x){
     return pow(x, 3) - pow(x, 2) + exp(x);
 };
 
 int main(){
-    std::cout << "----Secant Method----\n";
+    std::cout << "---- S E C A N T  M E T H O D ----\n"
+              << "Choose a function:\n"
+              << "\t1. f(x) = x^5 -21x^2 -8x^3 -4x^4 -28x + 60\n"
+              << "\t2. f(x) = x^3 - x^2 + e^x\n"
+              << "-> ";
+    int c;
+    std::cin >> c;
 
-    std::cout << "Choose function:"
-              << "\n1. f(x) = x^5 -21x^2 -8x^3 -4x^4 -28x + 60"
-              << "\n2. f(x) = x^3 - x^2 + e^x"
-              << "\n-> ";
-    int choice;
-
-    std::cin >> choice;
-
-    switch (choice)
+    switch(c)
     {
-    case 1:
-        std::cout << "f(x) = x^5 -21x^2 -8x^3 -4x^4 -28x + 60\n";
-        SecantMethod(f1);
-        break;
-    case 2:
-        std::cout << "f(x) = x^3 - x^2 + e^x\n";
-        SecantMethod(f2);
-        break;
-    default:
-        std::cout<< "byee!!!\n";
+        case 1:
+            SecantMethod(f1);
+            break;
+        case 2:
+            SecantMethod(f2);
+            break;
+        dafault:
         break;
     }
-
+    
+    std::cout << "\nBye bye!\n";
     return 0;
 }
