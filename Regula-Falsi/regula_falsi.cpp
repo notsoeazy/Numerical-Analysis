dde11f71a944133664a2b36fad5f6566f935a80c
notsoeazy <gonzalesjohnezekiel35@gmail.com>
2025-03-09 13:06:05 +0800

    fixed regula falsi

--- Regula-Falsi/regula_falsi.cpp
+++ Regula-Falsi/regula_falsi.cpp
@@ -2,132 +2,89 @@
 #include<cmath>
 #include<iomanip>
 
-class RegulaFalsi{
-public:
-    RegulaFalsi();
-    void SolveRoot(double (*f)(double));
+void RegulaFalsi(double (*f)(const double& )) {
+    double a, b, c, fa, fb, fc;
+    int itr = 0;
+    const double accuracy = 1e-10;
 
-private:
-    void SetInterval(double (*f)(double));
+    std::cout.precision(10);
+    std::cout.setf(std::ios::fixed);
 
-    double lowerInterval, upperInterval;
-    double Xa, Xb, Xr, XrPrev;
-    double fXa, fXb, fXr;
-    int itr;
-    double accuracy;
-};
+    do {
+        std::cout << "Enter lower interval: ";
+        std::cin >> a;
+        std::cout << "Enter upper interval: ";
+        std::cin >> b;
+    } while(
+        [](const double& n1, const double& n2) -> bool {
+            if(n1 * n2 > 0) {
+                std::cout << "No roots found in interval. Try again...\n";
+                return true;
+            } else {return false;};
+        }(f(a), f(b))
+    );
 
+    std::cout << std::setw(5) << "itr"
+              << std::setw(20) << "lower"
+              << std::setw(20) << "upper"
+              << std::setw(20) << "f(l)"
+              << std::setw(20) << "f(u)"
+              << std::setw(20) << "x"
+              << std::setw(20) << "fx\n";
 
-double function1 (double x){
+    do {
+        fa = f(a);
+        fb = f(b);
+        if(fa == 0) {
+            c = a;
+            break;
+        } else if(fb == 0) {
+            c = b;
+            break;
+        }
+        c = b - ((fb * (a - b)) / (fa - fb));
+        fc = f(c);
+        std::cout << std::setw(5) << ++itr
+                  << std::setw(20) << a
+                  << std::setw(20) << b
+                  << std::setw(20) << fa
+                  << std::setw(20) << fb
+                  << std::setw(20) << c
+                  << std::setw(20) << fc << "\n";
+        (fa * fc) < 0 ? b = c : a = c;
+    } while(fabs(fc) > accuracy);
+    std::cout << "The root is " << c << "\nFound after " << itr << " iterations.\n";
+}
+
+double f1(const double& x) {
     return x - exp(-1 * pow(x, 2));
 };
 
-double function2 (double x){
+double f2(const double& x) {
     return pow(x, 4) + (2 * pow(x, 3)) - (7 * pow(x, 2)) - (8 * x) + 12;
 };
 
-int main(){
-    std::cout << std::setprecision(10);
-    std::cout << std::fixed;
-
-    std::cout << "--- REGULA FALSI METHOD ---\n";
-
-    std::cout << "Choose function:"
-              << "\n1. f(x) = x - e^(-x^2)"
-              << "\n2. f(x) = x^4 + 2x^3 - 7x^2 - 8x + 12"
-              << "\n-> ";
-
-    RegulaFalsi solution;
-    int choice = 0;
-    std::cin >> choice;
-
-    if(choice == 1){
-        std::cout << "f(x) = x - e^(-x^2)" << std::endl;
-        solution.SolveRoot(function1);
+int main() {
+    std::cout << "---- R E G U L A - F A L S I  M E T H O D ----\n"
+              << "Choose function:\n"
+              << "\t1. f(x) = x - e^(-x^2)\n"
+              << "\t2. f(x) = x^4 + 2x^3 - 7x^2 - 8x + 12\n"
+              << "-> ";
+    int c = 0;
+    std::cin >> c;
+    switch(c)
+    {
+        case 1:
+            std::cout << "\n1. f(x) = x - e^(-x^2)\n";
+            RegulaFalsi(f1);
+            break;
+        case 2:
+            std::cout << "\n2. f(x) = x^4 + 2x^3 - 7x^2 - 8x + 12\n";
+            RegulaFalsi(f2);
+            break;
+        default:
+            break;
     }
-    else if(choice == 2){
-        std::cout << "f(x) = x^4 + 2x^3 - 7x^2 - 8x + 12" << std::endl;
-        solution.SolveRoot(function2);
-    }
-    else {
-        std::cout << "\nExiting Program...";
-    }
-    std::cout << "\n---------------------------------------------------------------------------\n";
-};
-
-//FUNCTION DEFINITION
-
-RegulaFalsi::RegulaFalsi(){
-    itr = 0;
-    accuracy = 1e-10;
-    Xa = NAN;
-    Xb = NAN;
-    Xr = NAN;
-};
-
-void RegulaFalsi::SetInterval(double (*f)(double)){
-    while (true) {
-        std::cout << "Enter initial guess...\n";
-        std::cout << "Enter lower interval: ";
-        std::cin >> lowerInterval;
-        std::cout << "Enter upper interval: ";
-        std::cin >> upperInterval;
-
-        if(f(lowerInterval) * f(upperInterval) <= 0){
-            Xa = lowerInterval;
-            Xb = upperInterval;
-
-            break;
-        }
-        else{
-            std::cout << "Root does not fall into the interval, guess again...\n";
-        }
-    };
-};
-
-void RegulaFalsi::SolveRoot(double (*f)(double)){
-    SetInterval(f);
-
-    std::cout << "\n#itr" << "\t" << "Xa" << "\t\t\t" << "Xb" << "\t\t\t" << "fXa" << "\t\t\t" << "fXb" << "\t\t\t" << "Xr" << "\t\t\t" << "fXr" << std::endl;
-
-    do{
-        fXa = f(Xa);
-        fXb = f(Xb);
-
-        XrPrev = Xr;
-        Xr = (Xb - ((fXb * (Xa - Xb)) / (fXa - fXb)));
-        fXr = f(Xr);
-
-        std::cout << ++itr << "\t" << Xa << "\t\t" << Xb << "\t\t" << fXa << "\t\t" << fXb << "\t\t" << Xr << "\t\t" << fXr << std::endl;
-
-        // check if root
-        if(fXr == 0){
-            // std::cout << "Reached 0\n";
-            break;
-        }
-        // check if intervals are roots
-        if(fXa == 0){
-            Xr = Xa;
-            // std::cout << "fXa=0\n";
-            break;
-        }
-        if(fXb == 0){
-            Xr = Xb;
-            // std::cout << "fXb=0\n";
-            break;
-        }
-
-        //Swap intervals
-        if(fXr * fXa < 0){
-            Xb = Xr;
-        }
-        else if(fXr * fXa > 0){
-            Xa = Xr;
-        }
-
-    }while((itr > 1) ? fabs(XrPrev - Xr) > accuracy : true);
-
-    std::cout << "\nStarting interval: [" << lowerInterval << ", " << upperInterval << "]" << std::endl;
-    std::cout << "Number of Iterations: " << itr << std::endl;
-    std::cout << "The root is " << Xr << std::endl;
-};
+    std::cout << "Bye!\n";
+    return 0;
+}
